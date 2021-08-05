8045bfa3ca0c15b1a65bad641115f7fd66b8e011
Shikha Singh <32593425+SHIKHASINGH1506@users.noreply.github.com>
2021-08-05 09:58:10 +0530

    fields hidden functionality

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const errorMessage = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 const changeTable = document.querySelector("#change-table");
 const nextButton = document.querySelector("#next-button");
-hideFields();
+const cashGivenInput = document.querySelector(".cashGivenInput");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
@@ -14,11 +14,11 @@ nextButton.addEventListener("click", validateBillAmount);
 
 function validateBillAmount(){
     if(billAmount.value<=0){
-        hideFields();
         showMessage("Enter valid bill amount");
     }
     else{
         showFields();
+        hideMessage();
     }
 }
 
@@ -27,8 +27,7 @@ function validateBillAndCashAmount(){
     if(billAmount.value>0){
         if(+cashGiven.value >= +billAmount.value){
             showTable();
-            calculateReturnAmount(cashGiven.value - billAmount.value);
-            
+            calculateReturnAmount(cashGiven.value - billAmount.value);    
         }
         else{
             showMessage("Cash is less than bill, please enter right amount");
@@ -51,29 +50,20 @@ function calculateReturnAmount(returnAmount){
     }
 }
 
-function hideFields(){
-    cashGiven.style.display = "none";
-    checkButton.style.display = "none"; 
-    changeTable.style.display = "none";
-   // errorMessage.style.display = "none";
-}
-
 function showTable(){
     changeTable.style.display = "block";
 }
 
+function hideTable(){
+    changeTable.style.display = "none";
+}
+
 function showFields(){
-    console.log("Right method called");
-    cashGiven.style.display = "block";
-    checkButton.style.display = "block"; 
+    cashGivenInput.style.display="block";    
 }
 
 function hideMessage(){
     errorMessage.style.display = "none";
-}
-
-function hideTable(){
-    changeTable.style.display = "none";
 }
 
 function showMessage(message){
